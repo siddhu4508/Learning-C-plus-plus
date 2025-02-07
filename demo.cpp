99abc43a64d6563b4e7b7ee0a32f6fb4d46ad6a3
Siddartha Yadav Sangam Reddy <siddhuyadav508@gmail.com>
2025-02-07 21:31:49 +0530

    all

---
+++ demo.cpp
@@ -0,0 +1,15 @@
+// Write hello world in Cpp
+
+
+// addeding input output module named stream "iostream"
+#include <iostream>
+
+// defining main function to start the program
+int main() {
+
+    // Printing "Hello, World!" to console using std::cout
+    std::cout << "Hello, World!" << std::endl;
+
+    // waiting for user input to close the console window
+    return 0;
+}
